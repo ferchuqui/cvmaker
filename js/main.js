dcc249e03caefd2c7aef63b25ab1fe9ecb93dff1
Luis Fernando Farji <fernando.farji@gmail.com>
2021-09-11 09:28:37 -0300

    tamaño de impresion

--- js/main.js
+++ js/main.js
@@ -110,17 +110,17 @@ let opt = {
 
 // Function to call areaCv and Html2Pdf options 
 function generateResume(){
-    html2pdf(areaCv, opt)
+    html2pdf(areaCv, opt)}
 
 // When the button is clicked, it executes the three functions
-resumeButton.addEventListener('click', () => {
+    resumeButton.addEventListener('click', () => {
 
-    // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
-scaleCv()
+        // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
+        scaleCv()
 
-    // 2. The PDF is generated
-generateResume()
+        // 2. The PDF is generated
+        generateResume()
 
-    // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
-    setTimeout(removeScale, 5000)
-}
+        // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
+        setTimeout(removeScale, 5000)
+    })
